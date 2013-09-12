732c389af6705566a78ffea5286a911afd13c831
Adriel Paredes <adriel.paredes@gmail.com>
2013-09-12 10:15:05 -0300

    Se agrega un plugin de juguete

--- pame.rb
+++ pame.rb
@@ -4,15 +4,33 @@ require './behaviour/karma/karma'
 require './behaviour/google/google'
 
 config = YAML.load_file('./config/pame.yml')
-puts config["server"]
-puts config["channels"]
+puts config
+
+
+
+
+class Greeter
+  include Cinch::Plugin
+  #solo es una prueba, despues el comportamiento no va a ser este
+
+ 	set :prefix, //
+
+  	match "hola"
+
+  	def execute(m)
+  		debug "hola!"
+    	m.reply "Hola #{m.user.name}!"
+  	end
+
+end
+
 
 bot = Cinch::Bot.new do
   configure do |c|
     c.server = config["server"]
     c.channels = config["channels"]
     c.nick = "pame"
-    c.plugins.plugins = [Karma]
+    c.plugins.plugins = [Greeter]
   end
 end
 
