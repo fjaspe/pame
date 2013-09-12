ec9c5e2ece0633e5fb589bc340ff7d1ce819a5d8
Adriel Paredes <adriel.paredes@gmail.com>
2013-09-12 17:53:49 -0300

    Se agrega un minikarma

--- behaviour/karma/karma.rb
+++ behaviour/karma/karma.rb
@@ -1,6 +1,38 @@
 require 'cinch'
+require 'yaml'
 
 class Karma
   include Cinch::Plugin
 
+  set :prefix, //
+
+
+  def initialize(*args)
+  	super
+  	load
+  end
+
+
+  def load
+  	@database = Hash.new
+  end
+
+  def save(m)
+
+  end
+
+  match /([A-Z]|[a-z]|[0-9])+\+\+/
+
+  	def execute(m)
+  		#Es feo, modificar para que quede lindo :D
+  		item = m.message[0..-3]
+  		if @database[m.channel].nil?
+  			@database[m.channel] = Hash.new
+  		end
+  		if @database[m.channel][m.user].nil?
+  			@database[m.channel][m.user] = 0
+  		end
+  		 @database[m.channel][m.user] += 1
+    	m.reply "El karma de #{item} ahora es #{@database[m.channel][m.user]}"
+  	end
 end
--- pame.rb
+++ pame.rb
@@ -7,30 +7,12 @@ config = YAML.load_file('./config/pame.yml')
 puts config
 
 
-
-
-class Greeter
-  include Cinch::Plugin
-  #solo es una prueba, despues el comportamiento no va a ser este
-
- 	set :prefix, //
-
-  	match "hola"
-
-  	def execute(m)
-  		debug "hola!"
-    	m.reply "Hola #{m.user.name}!"
-  	end
-
-end
-
-
 bot = Cinch::Bot.new do
   configure do |c|
     c.server = config["server"]
     c.channels = config["channels"]
     c.nick = "pame"
-    c.plugins.plugins = [Greeter]
+    c.plugins.plugins = [Karma]
   end
 end
 
