586bfbcf78ac87435ef236e6db18319becb4b21b
Adriel Paredes <adriel.paredes@gmail.com>
2013-09-12 09:39:23 -0300

    Pame tiene configuracion ahora

--- pame.rb
+++ pame.rb
@@ -1,11 +1,16 @@
 require 'cinch'
+require 'yaml'
 require './behaviour/karma/karma'
 require './behaviour/google/google'
 
+config = YAML.load_file('./config/pame.yml')
+puts config["server"]
+puts config["channels"]
+
 bot = Cinch::Bot.new do
   configure do |c|
-    c.server = "irc.devel.redhat.com"
-    c.channels = ["#jasperland"]
+    c.server = config["server"]
+    c.channels = config["channels"]
     c.nick = "pame"
     c.plugins.plugins = [Karma]
   end
